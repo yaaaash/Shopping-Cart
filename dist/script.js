d6e2653814d2a80df7b7d98b6e005fe1e9645749
yaaaash <yashmanwani13@gmail.com>
2021-09-27 20:02:30 +0530

    Uploaded Files

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+var removeCartItemButtons=document.getElementsByClassName("remove");console.log(removeCartItemButtons);for(var i=0;i<removeCartItemButtons.length;i++){var button=removeCartItemButtons[i];button.addEventListener("click",(function(){console.log("clicked")}))}
+//# sourceMappingURL=script.js.map
